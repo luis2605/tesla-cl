2bd9588e30006977cafcf317ffc32063e13d5691
luis2605 <89785699+luis2605@users.noreply.github.com>
2022-05-27 11:40:19 +0200

    test2

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,48 +1,44 @@
-import React ,{useState}from 'react'
+import React, { useState } from "react";
 
-import styledComponents from 'styled-components'
-import Fade from 'react-reveal/Fade';
+import styledComponents from "styled-components";
+import Fade from "react-reveal/Fade";
 
+function Section({ title, description, leftBtnText, rightBtnText, image }) {
+  //unpacking the Object in its props.Cooler way of passing props.
 
-function Section({title ,description,leftBtnText,rightBtnText, image}) {   //unpacking the Object in its props.Cooler way of passing props.
-    
   return (
-<div id={title}>
-   <Wrap   bgImage={image}>
-      
-            <Fade bottom>
-                <ItemText>
-                    <h1><a href='#'>{title}</a></h1>
-                    <p>{description}</p>
-                </ItemText>
+    <div id={title}>
+      <Wrap bgImage={image}>
+        <Fade bottom>
+          <ItemText>
+            <h1>
+              <a href="#">{title}</a>
+            </h1>
+            <p>{description}</p>
+          </ItemText>
         </Fade>
         <Buttons>
-        <Fade bottom>
-                <ButtonGroup>
-                        <LeftBtn>
-                            {leftBtnText}
-                        </LeftBtn>
-                        {rightBtnText &&                         //if rightBtnText exists, only then render RightBtn. If not leave empty.
-                        <RightBtn>
-                            {rightBtnText}
-                        </RightBtn>}
-                </ButtonGroup>
-            </Fade>
-            <DownArrow src="/images/down-arrow.svg"/>
+          <Fade bottom>
+            <ButtonGroup>
+              <LeftBtn>{leftBtnText}</LeftBtn>
+              {rightBtnText && ( //if rightBtnText exists, only then render RightBtn. If not leave empty.
+                <RightBtn>{rightBtnText}</RightBtn>
+              )}
+            </ButtonGroup>
+          </Fade>
+          <DownArrow src={process.env.PUBLIC_URL+"/images/down-arrow.svg"}  />
         </Buttons>
-      
-   </Wrap>
-   </div>
-  )
-
+      </Wrap>
+    </div>
+  );
 }
 
-export default Section
+export default Section;
 
-const Wrap =styledComponents.div `                                              //styledComponents are ok . I have to get use to the sintaxys .
+const Wrap = styledComponents.div`                                              //styledComponents are ok . I have to get use to the sintaxys .
  width:100vw;
  height:100vh;
- background-image: ${ props => `url(/images/${props.bgImage})`};
+ background-image: ${(props) => `url(${props.bgImage})`};
  background-size: cover;
  background-position:center;
  background-repeat:no-repeat;
@@ -52,12 +48,12 @@ const Wrap =styledComponents.div `                                              
  justify-content:space-between;
  align-items:center;
 
-` 
+`;
 const ItemText = styledComponents.div`
  padding-top:15vh;
  text-align:center;
 
-`
+`;
 const ButtonGroup = styledComponents.div`
 display:flex;
 flex-direction:row;
@@ -65,7 +61,7 @@ flex-direction:row;
     flex-direction:column;
 }
 
-`
+`;
 const LeftBtn = styledComponents.div`
 background-color: var(--btn-background);
 width:15em;
@@ -82,18 +78,18 @@ font-size:.8rem;
 cursor:pointer;
 opacity:0.9;
 
-`
+`;
 
-const RightBtn =styledComponents(LeftBtn)`
+const RightBtn = styledComponents(LeftBtn)`
 background:white;
 color:var(--color-font)
 
-`
+`;
 
 const DownArrow = styledComponents.img`
 margin:.5em 0 .5em 0;
 height:3em;
 
 animation:animateDown infinite 1.5s;
-`
-const Buttons =styledComponents.div``
+`;
+const Buttons = styledComponents.div``;
